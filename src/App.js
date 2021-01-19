eb2916f01ed4345141ac496db97d5c8b259265ce
bella <bella.royle@gmail.com>
2021-01-19 17:35:49 +0000

    adds buttons and router to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React from 'react';
+import { Router } from '@reach/router';
 
 import Header from './components/Header';
+import Buttons from './components/Buttons';
+import Pong from './components/Pong';
 
 function App() {
   return (
     <div>
       <Header />
+      <Buttons />
+      <Router>
+        <Pong path="/pong" />
+      </Router>
     </div>
   );
 }
