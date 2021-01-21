19d9cc8d07fc5ea175f49f8fe395fa887e0d0a69
bella <bella.royle@gmail.com>
2021-01-21 18:57:13 +0000

    changed button color

--- src/components/miniComponents.js
+++ src/components/miniComponents.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 export const Button = styled.button`
-  background-color: #88d18a;
+  background-color: #66ff33;
   padding: 5px;
   margin: 10px;
-  border: 1px solid #096b72;
+  border: 2px solid #096b72;
   border-radius: 10px;
   color: black;
   box-shadow: 5px 1px 15px 1px #2e294e80;
